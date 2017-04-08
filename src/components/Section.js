c677b81a844aae1a3ae5445d0282b59791bc0a40
Sylvain <bopulie@gmail.com>
2017-04-08 17:50:49 +0200

    Add showcase for projects

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Showcase from "./Showcase";
 
 const Section = ({ name, title, text }) => (
   <section className={`sb-section ${name}`}>
     <article>
       <h1>{title}</h1>
       {text.map(para => <p key={para}>{para}</p>)}
+      {name === "code" ? <Showcase /> : null}
     </article>
   </section>
 );
---
+++ src/components/Showcase.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Showcase = () => (
+  <div className="showcase">
+    <div className="project">
+      <div className="project-infos" />
+    </div>
+    <div className="project">
+      <div className="project-infos" />
+    </div>
+    <div className="project">
+      <div className="project-infos" />
+    </div>
+  </div>
+);
+
+export default Showcase;
