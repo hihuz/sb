7aaf02aa0e92362c2f9868014c8892333f91237e
Sylvain <bopulie@gmail.com>
2017-04-09 15:31:02 +0200

    Update Showcase.js

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,7 +3,23 @@ import React from "react";
 const Showcase = () => (
   <div className="showcase">
     <div className="project">
-      <div className="project-infos" />
+      <div className="project-infos">
+        <h2 className="project-name">Meny</h2>
+        <p className="project-desc">
+          A recipe sharing / editing app, work in progress
+        </p>
+        <div className="project-logos">
+          <div className="project-tech">
+            <img src="../public/react.png" alt="react" />React
+          </div>
+          <div className="project-tech">
+            <img src="../public/redux.png" alt="redux" />Redux
+          </div>
+          <div className="project-tech">
+            <img src="../public/firebase.png" alt="firebase" />Firebase
+          </div>
+        </div>
+      </div>
     </div>
     <div className="project">
       <div className="project-infos" />
