704176537b073525040f0c66a6a483e0249398da
Sylvain Boulade <bopulie@gmail.com>
2017-04-12 20:27:51 +0200

    Remove localStorage for greetings
    I wasn't using this, I may put this back later but for now this is dead code

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,37 +1,21 @@
 import React from "react";
 import LinkBar from "./LinkBar";
-import { loadState, saveState } from "../utils/localStorage";
 
-// The updated greeting on revisits is disabled for now but i'm keeping the logic here for later
-class Intro extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visited: loadState()
-    };
-  }
-  componentDidMount() {
-    saveState(1);
-  }
-  render() {
-    const { title, text, greetings } = this.props;
-    return (
-      <section className="sb-intro">
-        <article className="intro">
-          <h1 className="intro-title">{title}</h1>
-          <div className="logo-container">
-            <img src="hihuz.jpg" alt="hihuz" />
-          </div>
-          {text.map(para => {
-            const markup = { __html: para };
-            return <h4 className="intro-text" key={para} dangerouslySetInnerHTML={markup} />;
-          })}
-          <h4 className="intro-text">{greetings[0]}</h4>
-          <LinkBar />
-        </article>
-      </section>
-    );
-  }
-}
+const Intro = ({ title, text, greetings }) => (
+  <section className="sb-intro">
+    <article className="intro">
+      <h1 className="intro-title">{title}</h1>
+      <div className="logo-container">
+        <img src="hihuz.jpg" alt="hihuz" />
+      </div>
+      {text.map(para => {
+        const markup = { __html: para };
+        return <h4 className="intro-text" key={para} dangerouslySetInnerHTML={markup} />;
+      })}
+      <h4 className="intro-text">{greetings[0]}</h4>
+      <LinkBar />
+    </article>
+  </section>
+);
 
 export default Intro;
