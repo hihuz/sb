8985b4709edbe0b5eb0d7e20fc01ada3ab4438e4
Sylvain Boulade <bopulie@gmail.com>
2017-04-12 10:30:44 +0200

    Add a light box-shadow to projects, fix Footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import content from "../content/content.json";
 
-const Footer = () => <footer dangerouslySetInnerHTML={{ __html: content.footer }} />;
+const Footer = () => <footer>{content.footer}</footer>;
 
 export default Footer;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,3 @@ import Page from "./components/Page";
 OfflinePluginRuntime.install();
 
 render(<Page />, document.getElementById("page"));
-
-/*
-- add a more visible separation for showcase projects : turn the images to grey instead of full colors ?
-- test / verify the added colors
-- add a slight bit of accent color on the main screen, and maybe on the showcase projects ?
-*/
