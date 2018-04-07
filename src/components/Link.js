079715492807c043f8ee0e42ae9129e84ce17ae8
Sylvain Boulade <bopulie@gmail.com>
2018-04-07 18:03:14 +0200

    Improve a11y

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Link = ({ url, viewbox, path }) => (
+const Link = ({ url, name, viewbox, path }) => (
   <div className="link-container">
-    <a href={url}>
-      <svg viewBox={viewbox}>
+    <a href={url} title={name}>
+      <svg viewBox={viewbox} role="presentation" focusable="false">
         <path d={path} />
       </svg>
     </a>
